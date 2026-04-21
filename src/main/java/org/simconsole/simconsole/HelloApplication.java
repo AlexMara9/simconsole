1e229fdb9a2dd9aaf8431b69dd9f314e4bedbeb5
AlexMara9 <example@example.com>
2026-04-21 18:21:53 +0200

    change scene size

--- src/main/java/org/simconsole/simconsole/HelloApplication.java
+++ src/main/java/org/simconsole/simconsole/HelloApplication.java
@@ -11,8 +11,9 @@ public class HelloApplication extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("console-view.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+		Scene scene = new Scene(fxmlLoader.load(), (int)(1920/3), (int)(1080/3));
 		stage.setTitle("SimConsole");
+		stage.resizableProperty().setValue(false);
 		stage.setScene(scene);
 		stage.show();
 	}
