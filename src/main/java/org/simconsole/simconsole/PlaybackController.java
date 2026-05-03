631e64103a874530fb44f1522015f5db6d6d1e65
AlexMara9 <example@example.com>
2026-05-04 00:37:42 +0200

    add playback container padding

--- src/main/java/org/simconsole/simconsole/PlaybackController.java
+++ src/main/java/org/simconsole/simconsole/PlaybackController.java
@@ -4,8 +4,10 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.binding.DoubleBinding;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 
 public class PlaybackController {
 	/* responsive */
@@ -13,6 +15,8 @@ public class PlaybackController {
 	private static final double BUTTON_CONTAINER_SPACING_FACTOR = 0.125;// compared to the HBox
 	private static final double ICON_SIZE_FACTOR = 0.6;
 
+	@FXML private StackPane playbackContainer;
+	@FXML private GridPane playbackControlsContainer;
 	@FXML private HBox buttonsContainer;
 	@FXML private Button unskipButton;
 	@FXML private Button playButton;
@@ -23,6 +27,10 @@ public class PlaybackController {
 		responsive();
 	}
 	private void responsive(){
+
+		playbackControlsContainer.prefHeightProperty().bind(playbackContainer.heightProperty().multiply(0.8));
+		playbackControlsContainer.prefWidthProperty().bind(playbackContainer.widthProperty().multiply(0.8));
+
 		Button [] buttons = {unskipButton, playButton, skipButton};
 
 		for (Button b : buttons){
