acbe5dd03e05072a24abafdddc48d065979d8835
AlexMara9 <example@example.com>
2026-07-13 21:01:47 +0200

    add pan control

--- src/main/java/org/simconsole/simconsole/MixerController.java
+++ src/main/java/org/simconsole/simconsole/MixerController.java
@@ -6,6 +6,9 @@ public class MixerController {
 
     @FXML private DynamicSlider leftVolumeSlider;
     @FXML private DynamicSlider rightVolumeSlider;
+    
+    @FXML private LineKnob leftPanKnob;
+    @FXML private LineKnob rightPanKnob;
 
     private Deck leftDeck;
     private Deck rightDeck;
@@ -19,11 +22,25 @@ public class MixerController {
                 }
             });
         }
+        if (leftPanKnob != null) {
+            leftPanKnob.valueProperty().addListener((obs, oldVal, newVal) -> {
+                if (leftDeck != null) {
+                    leftDeck.getControls().setPan(-newVal.doubleValue());
+                }
+            });
+        }
         
         if (rightVolumeSlider != null) {
             rightVolumeSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
                 if (rightDeck != null) {
                     rightDeck.getControls().setVolume(newVal.doubleValue() / 100.0);
+                }
+            });
+        }
+        if (rightPanKnob != null) {
+            rightPanKnob.valueProperty().addListener((obs, oldVal, newVal) -> {
+                if (rightDeck != null) {
+                    rightDeck.getControls().setPan(-newVal.doubleValue());
                 }
             });
         }
@@ -34,11 +51,13 @@ public class MixerController {
         this.rightDeck = rightDeck;
         
         // Inizializza i valori correnti (dal range 0.0-1.0 al range slider 0-100)
-        if (this.leftDeck != null && leftVolumeSlider != null) {
-            leftVolumeSlider.setValue(this.leftDeck.getControls().getVolume() * 100.0);
+        if (this.leftDeck != null) {
+            if (leftVolumeSlider != null) leftVolumeSlider.setValue(this.leftDeck.getControls().getVolume() * 100.0);
+            if (leftPanKnob != null) leftPanKnob.setValue(-this.leftDeck.getControls().getPan());
         }
-        if (this.rightDeck != null && rightVolumeSlider != null) {
-            rightVolumeSlider.setValue(this.rightDeck.getControls().getVolume() * 100.0);
+        if (this.rightDeck != null) {
+            if (rightVolumeSlider != null) rightVolumeSlider.setValue(this.rightDeck.getControls().getVolume() * 100.0);
+            if (rightPanKnob != null) rightPanKnob.setValue(-this.rightDeck.getControls().getPan());
         }
     }
 }
