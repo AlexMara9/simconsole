0b5c59b197d7756e3e5310e56cfc9f11b89a4f4a
AlexMara9 <example@example.com>
2026-07-15 19:57:16 +0200

    revert playbutton icon change

--- src/main/java/org/simconsole/simconsole/PlayButtonSkin.java
+++ src/main/java/org/simconsole/simconsole/PlayButtonSkin.java
@@ -10,22 +10,5 @@ public class PlayButtonSkin extends LedButtonRoundSkin {
 
     public PlayButtonSkin(PlayButton control) {
         super(control);
-
-        control.selectedProperty().addListener((obs, oldVal, isPlaying) -> {
-            updateIconState(control, isPlaying);
-        });
-
-        control.graphicProperty().addListener((obs, oldVal, newVal) -> {
-            updateIconState(control, control.isSelected());
-        });
-        updateIconState(control, control.isSelected());
-    }
-
-    private void updateIconState(PlayButton control, boolean isPlaying) {
-        Node graphic = control.getGraphic();
-        if (graphic != null) {
-            graphic.getStyleClass().removeAll("icon-play", "icon-pause");
-            graphic.getStyleClass().add(isPlaying ? "icon-pause" : "icon-play");
-        }
     }
 }
